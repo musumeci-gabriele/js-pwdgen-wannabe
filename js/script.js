e381e50c158350c5dba76b6e989391df31186425
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-03 16:08:38 +0200

    js in costruzione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert("pw sola !");
+// AVVERTIMENTO PER L'UTENTE
+alert("Questo sito ti rubera' tutti i tuoi dati. Grazie di aver preso visione, ora procedi.");
+
+// INSERIMENTO NOME UTENTE
+var nome = prompt("Inserisci il tuo nome :");
+
+
+// INSERIMENTO COGNOME
+var nome = prompt("Inserisci il tuo cognome :");
+
+// INSERIMENTO COLORE PREFERITO
+var nome = prompt("Inserisci il tuo colore preferito :");
+
+// INSERIMENTO ANNO DI NASCITA
+var nome = parInt(prompt("Inserisci il tuo anno :"));
+
+// GENERAZIONE PW SOLA
